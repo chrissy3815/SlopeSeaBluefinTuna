7f5584a34f78e101a5dc34c1274a42c546a7a468
Christina Hernandez <chrissy@Christinas-MacBook-Air.local>
2020-09-17 11:13:35 -0400

    Updates to the larval simulation figure after Dave's comments: (1) split it into two panels shows backtracking and forward tracking, and (2) switch out Irina's Slope Sea bounding box for the boundary shapefile provided by Richardson et al. 2016

--- SlopeSea_backtracking_results.R
+++ SlopeSea_backtracking_results.R
@@ -5,6 +5,7 @@ library(rmatio)
 library(oce)
 library(ocedata)
 library(ncdf4)
+library(rgdal)
 
 # Color palette that is colorblind-accessible:
 # Grey, orange, light blue, green, yellow, dark blue, red, pink, black
@@ -32,14 +33,18 @@ lon<- ncvar_get(ncid, varid='lon')
 elev<- ncvar_get(ncid, varid='elevation')
 nc_close(ncid)
 # load in the Slope Sea boundary:
-SS_polygon<- read.mat('data/LonLatSlopeSea.mat')
+#SS_polygon<- read.mat('data/LonLatSlopeSea.mat') # Irina's SlopeSea boundary
+SS_shp<- readOGR('data/gazetteer_polygon/gazetteer_polygon.shp') #Dave's SlopeSea boundary
 
-png(filename='results/SS2016_trajectory_map.png', height=5.5, width=7, units='in', res=300)
+# make a plot of the backtracked locations:
+setEPS()
+postscript('results/SS2016_trajectory_map_backwards.eps', height=5.5, width=7)
 plot(coastlineWorldFine, longitudelim=c(-60, -77), latitudelim=c(33, 43), 
      xlab='Longitude', ylab='Latitude')
 contour(lon, lat, elev, levels=c(-100, -200, -1000, -2000), add=TRUE, 
         drawlabels=FALSE, lwd=0.75, col=cbPalette[1])
-polygon(SS_polygon$lonss, SS_polygon$latss, border= cbPalette[4], lwd=1.5)
+# polygon(SS_polygon$lonss, SS_polygon$latss, border= cbPalette[5], lwd=1.5) # for adding Irina's boundary
+plot(SS_shp, border=cbPalette[2], lwd=1.5, add=TRUE)
 
 for (i in 1:68){
   thisfile<- paste('data/MABGOM2_2016_tr/MABGOM2_2016_tr',i,'.mat', sep='')
@@ -48,6 +53,26 @@ for (i in 1:68){
   lines(traj$lontr_b, traj$lattr_b, col=cbPalette[6])
   n<- length(traj$lontr_b)
   points(traj$lontr_b[n], traj$lattr_b[n], pch=24, col=cbPalette[6], bg=cbPalette[6])
+}
+legend("bottomright", legend=c('collection', 'backtracked origin'),
+       col=c('black', cbPalette[6]), pch=c(19,24), 
+       pt.bg=c('black', cbPalette[6]))
+dev.off()
+
+# make a plot of the forward-tracked locations:
+setEPS()
+postscript('results/SS2016_trajectory_map_forwards.eps', height=5.5, width=7)
+plot(coastlineWorldFine, longitudelim=c(-60, -77), latitudelim=c(33, 43), 
+     xlab='Longitude', ylab='Latitude')
+contour(lon, lat, elev, levels=c(-100, -200, -1000, -2000), add=TRUE, 
+        drawlabels=FALSE, lwd=0.75, col=cbPalette[1])
+# polygon(SS_polygon$lonss, SS_polygon$latss, border= cbPalette[5], lwd=1.5) # for adding Irina's boundary
+plot(SS_shp, border=cbPalette[2], lwd=1.5, add=TRUE)
+
+for (i in 1:68){
+  thisfile<- paste('data/MABGOM2_2016_tr/MABGOM2_2016_tr',i,'.mat', sep='')
+  traj<- read.mat(thisfile)
+  points(traj$lontr_b[1], traj$lattr_b[1], pch=19)
   lines(traj$lontr_f, traj$lattr_f, col=cbPalette[3], lwd=0.5)
   n<- length(traj$lontr_f)
   if (is.na(traj$lattr_f[n])){
@@ -56,10 +81,11 @@ for (i in 1:68){
   }
   points(traj$lontr_f[n], traj$lattr_f[n], pch=25, col=cbPalette[3], bg=cbPalette[3])
 }
-legend("bottomright", legend=c('collection', 'backtracked origin', '25 days post spawn'),
-       col=c('black', cbPalette[6], cbPalette[3]), pch=c(19,24,25), 
-       pt.bg=c('black', cbPalette[6], cbPalette[3]))
+legend("bottomright", legend=c('collection', '25 days post spawn'),
+       col=c('black', cbPalette[3]), pch=c(19,25), 
+       pt.bg=c('black', cbPalette[3]))
 dev.off()
+
 
 # the ones that left the domain:
 leftdomain<- c(25, 44, 45, 46, 47, 48, 49)
@@ -68,7 +94,7 @@ plot(coastlineWorldFine, longitudelim=c(-60, -77), latitudelim=c(33, 43),
      xlab='Longitude', ylab='Latitude')
 contour(lon, lat, elev, levels=c(-100, -200, -1000, -2000), add=TRUE, 
         drawlabels=FALSE, lwd=0.75, col=cbPalette[1])
-polygon(SS_polygon$lonss, SS_polygon$latss, border= cbPalette[4], lwd=1.5)
+polygon(SS_polygon$lonss, SS_polygon$latss, border= cbPalette[5], lwd=1.5)
 
 for (i in leftdomain){
   thisfile<- paste('data/MABGOM2_2016_tr/MABGOM2_2016_tr',i,'.mat', sep='')
